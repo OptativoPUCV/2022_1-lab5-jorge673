d5df2344f01d86a76a45595045ff93eb3ff216f9
JorgePalacios3 <>
2022-04-21 23:52:26 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -46,7 +46,8 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
 
 
 void insertTreeMap(TreeMap * tree, void* key, void * value) {
-  searchTreeMap(tree,key);
+  printf("error?");
+  /*searchTreeMap(tree,key);
 
   if(tree->root==tree->current)printf("si--------------");
   else printf("no---------");
@@ -64,7 +65,7 @@ void insertTreeMap(TreeMap * tree, void* key, void * value) {
       new->parent=aux;
     }
 
-  tree->current=new;
+  tree->current=new;*/
   
 }
 
