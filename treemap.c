b20449218ab75037a4d414acb912505850a3bffa
JorgePalacios3 <>
2022-04-21 23:41:54 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -48,7 +48,7 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
 void insertTreeMap(TreeMap * tree, void* key, void * value) {
   searchTreeMap(tree,key)
 
-  treeNode* aux=tree->current
+  treeNode* aux=tree->current;
   treeNode* new=createTreeNode(key,value);
 
   if(tree->lower_than(new,aux->pair->key)){
