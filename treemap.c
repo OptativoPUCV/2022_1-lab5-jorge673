b1d317aeb425d903ed5071b17e3f3e84b718d932
JorgePalacios3 <>
2022-04-22 23:31:40 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -51,10 +51,10 @@ void insertTreeMap(TreeMap * tree, void* key, void * value) {
   //TreeNode* nuevo=createTreeNode(tree,value);
 
   if(aux->left==NULL){
-    return 1
+    return 1;
   }
   if(aux->right==NULL){
-    return 1
+    return 1;
   }
   
 
